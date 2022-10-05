c8f4ba95a5e32e575f28f530895360eedaf512d8
Bhumit Rohilla <adf@example.com>
2022-10-05 23:05:19 +0530

    Updated stopwatch app

--- Stopwatch/app/src/main/java/com/example/stopwatch/MainActivity.kt
+++ Stopwatch/app/src/main/java/com/example/stopwatch/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.stopwatch
 import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.os.SystemClock
 import android.view.View
 import android.widget.Button
@@ -10,15 +11,24 @@ import android.widget.Chronometer
 
 class MainActivity : AppCompatActivity() {
     @SuppressLint("MissingInflatedId")
+    var offset:Long=0
+    var status=false
+    lateinit var timer:Chronometer
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val timer=findViewById<Chronometer>(R.id.stopwatch)
+        timer=findViewById<Chronometer>(R.id.stopwatch)
         val start=findViewById<Button>(R.id.start)
         val pause=findViewById<Button>(R.id.pause)
         val reset=findViewById<Button>(R.id.reset)
-        var offset:Long=0
-        var status=false
+        if(savedInstanceState!=null){
+            offset=savedInstanceState.getLong("offset_key")
+            status=savedInstanceState.getBoolean("status")
+            if(status){
+                timer.base=savedInstanceState.getLong("base_key")
+                timer.start()
+            }
+        }
         start.setOnClickListener {
             if(!status){
                 timer.base=SystemClock.elapsedRealtime()-offset
@@ -40,5 +50,12 @@ class MainActivity : AppCompatActivity() {
         reset.setOnClickListener {
             timer.base=SystemClock.elapsedRealtime()
         }
+    }
+
+    override fun onSaveInstanceState(saveState:Bundle){
+        super.onSaveInstanceState(saveState)
+        saveState.putLong("offset_key",offset)
+        saveState.putLong("base_key",timer.base)
+        saveState.putBoolean("status",status)
     }
 }
