5d66e396eaaba6e326ebb55acd0778aebf11750b
Bhumit Rohilla <adf@example.com>
2022-10-15 00:25:46 +0530

    Files are added

--- CatChat/app/src/main/java/com/example/catchat/MainActivity.kt
+++ CatChat/app/src/main/java/com/example/catchat/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Button
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
@@ -13,6 +14,7 @@ import androidx.navigation.ui.onNavDestinationSelected
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.appbar.MaterialToolbar
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,13 +25,16 @@ class MainActivity : AppCompatActivity() {
         val navHostFragment=supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
         val navController=navHostFragment.navController
         val builder=AppBarConfiguration.Builder(navController.graph)
+        val drawer:DrawerLayout=findViewById(R.id.drawer_layout)
+        builder.setOpenableLayout(drawer)
         val appBarConfiguration=builder.build()
         toolbar.setupWithNavController(navController,appBarConfiguration)
 
         val bottonNav=findViewById<BottomNavigationView>(R.id.bottonNav)
         bottonNav.setupWithNavController(navController)
 
-
+        val navDrawer=findViewById<NavigationView>(R.id.nav_view)
+        navDrawer.setupWithNavController(navController)
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
